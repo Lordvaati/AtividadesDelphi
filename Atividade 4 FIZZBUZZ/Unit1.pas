6bdb8b7ce56cd67ad3cb7d8db20fe38d15076eb6
Gabriell Cruz <gabriell.cruz666@gmail.com>
2021-10-17 20:45:08 -0300

    Atividade 4

---
+++ Atividade 4 FIZZBUZZ/Unit1.pas
@@ -0,0 +1,58 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Edit1: TEdit;
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  I, valor: integer;
+begin
+
+ valor:= StrToInt(Edit1.Text);
+ for I := 1 to valor do
+  begin
+    if I mod 3 = 0 then
+      begin
+        Memo1.Lines.Add(IntToStr(i) + ': FIZZ!');
+      end
+    else if I mod 5 = 0 then
+      begin
+        Memo1.Lines.Add(IntToStr(i) + ': BUZZ!');
+      end
+    else if (I mod 3 = 0) AND (I mod 5 = 0) then
+      begin
+        Memo1.Lines.Add(IntToStr(i) + ': FIZZBUZZ!');
+      end
+    else
+      begin
+        Memo1.Lines.Add(IntToStr(I));
+      end;
+
+  end;
+
+end;
+
+end.
